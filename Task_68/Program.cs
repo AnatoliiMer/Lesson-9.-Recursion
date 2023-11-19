b33a75206656b0244ad08ae5f38aa93582f28b06
Anatolii <tumerovo@yandex.ru>
2023-11-19 20:59:07 +0500

    Task 68

---
+++ Task_68/Program.cs
@@ -0,0 +1,23 @@
+﻿int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else if (m > 0 && n > 0)
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+        return 0;
+    }
+
+    
+    {
+        int result = Ackermann(3, 4);
+        Console.WriteLine(result);  // Вывод: 125
+    }
+
