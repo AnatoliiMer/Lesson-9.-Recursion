f3f2893da5f248c98e245eca9964118d9cd0838e
Anatolii <tumerovo@yandex.ru>
2023-11-19 20:51:35 +0500

    Task 66

---
+++ Task_66/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите значение M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите значение N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int sum = CalculateSumOfNaturalNumbers(m, n);
+    Console.WriteLine($"Сумма натуральных чисел от {m} до {n} равна {sum}");
+  
+    int CalculateSumOfNaturalNumbers(int current, int end)
+  {
+    if (current > end)
+    {
+      return 0;
+    }
+    return current + CalculateSumOfNaturalNumbers(current + 1, end);
+  }
